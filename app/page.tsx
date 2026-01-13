76be5fcf0d238067fd0fa7311e5153da5eb804ed
sonamdorjelama1999 <sonamdorjelama1999@gmail.com>
2026-01-13 17:45:04 +0545

    v3

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import DomeGallery from '@/components/DomeGallery';
+
+export default function Home() {
+  return (
+    <div className='bg-black' style={{ width: '100vw', height: '100vh' }}>
+      <DomeGallery />
+    </div>
+  );
+}
